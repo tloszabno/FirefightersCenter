e3ea9b112444e72d0e81a963a96736c1cfeffcbd
tomek <tomek@gmail.com>
2015-04-19 20:42:58 +0200

    [FirefightersPostApp] init project

---
+++ FirefightersPostApp/src/main/java/Library.java
@@ -0,0 +1,10 @@
+public class Library {
+
+    public static void main(String[] args) {
+        System.out.println("FirefightersPostApp");
+    }
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
