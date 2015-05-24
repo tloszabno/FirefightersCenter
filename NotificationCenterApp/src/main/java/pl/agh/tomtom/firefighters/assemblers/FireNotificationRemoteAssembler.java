495508d5d29c20ad53aa7158e6a9f3bf4e57201b
Tomasz Dziok <tomaszdziok@gmail.com>
2015-05-25 00:39:10 +0200

    send notification id

--- NotificationCenterApp/src/main/java/pl/agh/tomtom/firefighters/assemblers/FireNotificationRemoteAssembler.java
+++ NotificationCenterApp/src/main/java/pl/agh/tomtom/firefighters/assemblers/FireNotificationRemoteAssembler.java
@@ -2,35 +2,38 @@ package pl.agh.tomtom.firefighters.assemblers;
 
 import generated.integracja.common.dto.FireNotificationIDTO;
 import generated.integracja.common.dto.OrderedResource;
+import org.apache.commons.lang3.StringUtils;
+import pl.agh.tomtom.firefighters.dto.FireNotificationDTO;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-
-import pl.agh.tomtom.firefighters.dto.FireNotificationDTO;
-
 public class FireNotificationRemoteAssembler {
-	public static FireNotificationIDTO toRemote(FireNotificationDTO dto) {
+  public static FireNotificationIDTO toRemote(FireNotificationDTO dto) {
 
-		FireNotificationIDTO idto = new FireNotificationIDTO().withActionName(dto.getType())
-				.withAddress(dto.getAddress()).withCity(dto.getCity()).withCreator(dto.getCreator())
-				.withDescription(dto.getDescription()).withLatitude(safeDoubleToString(dto.getGpsXCoordinate()))
-				.withLongitude(safeDoubleToString(dto.getGpsYCoordinate()))
-				.withNotifiactionDate(dto.getNotificationDate()).withType(dto.getType());
+    FireNotificationIDTO idto = new FireNotificationIDTO()
+        .withNotificationId(dto.getId())
+        .withActionName(dto.getType())
+        .withAddress(dto.getAddress())
+        .withCity(dto.getCity())
+        .withCreator(dto.getCreator())
+        .withDescription(dto.getDescription()).withLatitude(safeDoubleToString(dto.getGpsXCoordinate()))
+        .withLongitude(safeDoubleToString(dto.getGpsYCoordinate()))
+        .withNotifiactionDate(dto.getNotificationDate()).withType(dto.getType());
 
-		// FIXME:
-		idto.withRequestedFirefightersUnitNumber(1);
-		List<OrderedResource> orderedResources = new ArrayList<>();
-		idto.withOrderedResources(orderedResources);
+    // FIXME:
+    idto.withRequestedFirefightersUnitNumber(1);
 
-		return idto;
-	}
+    List<OrderedResource> orderedResources = new ArrayList<>();
+    idto.withOrderedResources(orderedResources);
 
-	private static String safeDoubleToString(Double d) {
-		if (d == null) {
-			return StringUtils.EMPTY;
-		}
-		return d.toString();
-	}
+    return idto;
+  }
+
+  private static String safeDoubleToString(Double d) {
+    if (d == null) {
+      return StringUtils.EMPTY;
+    }
+    return d.toString();
+  }
 }
