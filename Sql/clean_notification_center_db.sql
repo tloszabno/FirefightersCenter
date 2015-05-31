852e7922d3625b9308481627af702a30c4285631
Tomasz Los <tloszabno@gmail.com>
2015-05-31 11:25:58 +0200

    Added sql script to clean notifiaction center db

---
+++ Sql/clean_notification_center_db.sql
@@ -0,0 +1,8 @@
+delete from equipment_report_entry;
+delete from extinguishing_stuff_report_entry;
+delete from firefighters_brigadereport_entry;
+delete from firefighters_posts;
+delete from firefighter_report_entry;
+delete from reports;
+delete from firefighters_post;
+delete from fire_notification;
